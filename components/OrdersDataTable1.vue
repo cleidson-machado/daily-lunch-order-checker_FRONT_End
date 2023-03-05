df7292cea78855063d9ac51e4780c9aad96fb86e
Cleidson <cleidson.mac@outlook.com>
2023-03-05 13:06:15 -0400

    Just to Save the Methods used when I was try Manipulate date time stuff

--- components/OrdersDataTable1.vue
+++ components/OrdersDataTable1.vue
@@ -7,7 +7,7 @@
                     Lunch Box Name
                 </th>
                 <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
-                    Order Price
+                    Order Day Creation
                 </th>
                 <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
                     Client First Name
@@ -20,16 +20,16 @@
         <tbody>
             <tr v-for="order in ordersList" v-bind:key="order.id" >
                 <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                    <span>{{ order.lunchMeal.lunchBox.name }}</span>
+                    <span>{{ toTheUpperCase (order.lunchMeal.lunchBox.name) }}</span>
                 </td>
                 <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                    <span>{{ todayDay }} / {{ todayMonth }} / {{ todayYear }}</span>
+                    <span>{{ formatDateDayJs(order.createdAt) }}</span>
                 </td>
                 <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                    <span>{{ order.userOrder.firstName }}</span>
+                    <span>{{ toTheUpperCase(order.userOrder.firstName) }}</span>
                 </td>
                 <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                    <span>{{ order.lunchMeal.name }}</span>
+                    <span>{{ toTheUpperCase (order.lunchMeal.name) }}</span>
                 </td>
             </tr>
         </tbody>
@@ -38,6 +38,7 @@
 
   <script lang="ts">
   import Vue from 'vue'
+  import * as dayjs from 'dayjs'
 
   export default Vue.extend({
     name: 'OrderTable',
@@ -64,6 +65,8 @@
       async fetchOrdersData() {
         const orders = this.$axios.$get('/foodapi/order-for-lunch/listAll');
             this.ordersList = await orders;
+
+            //TEST
             console.table(orders);
             console.log('METODO 1 Date:' + this.todayDay, this.todayMonth , this.todayYear); // NÃO ESTÁ TRAZENDO O NÚMERO REF. AO MÊS CORRETO!.. VERIFICAR!
         },
@@ -78,6 +81,27 @@
             const d = new Date(Date.now());
             var dayName = days[d.getDay()];
             console.log('METODO 3 Date:' + dayName);
+        },
+
+        formatDate(givendate) {
+            const theOne = givendate.replace(/T|Z/g, ' ');
+        return theOne
+        },
+
+        formatDateDayJs(givendate){
+            //const theOne = dayjs(givendate).format("MMMM, YYYY");
+            //const theOne = dayjs(givendate).format("MM/DD/YYYY");
+            //const theOne = dayjs(givendate).format("DD/MM/YYYY");
+            //const theOne = dayjs(givendate).format("DD/MMMM/YYYY");
+            //const theOne = dayjs(givendate).format("dddd/MMMM/YYYY");//ESSE PODE SER UTIL.. ADAPTAR AO SALVAR O PEDIDO?
+            const theOne = dayjs(givendate).format("dddd");//ESSE PODE SER UTIL.. ADAPTAR AO SALVAR O PEDIDO?
+            //const theOne = dayjs(givendate).toString();
+        return theOne.toUpperCase()
+        },
+
+        toTheUpperCase(theField){
+            const theOne = theField
+            return theOne.toUpperCase()
         }
     }
 
