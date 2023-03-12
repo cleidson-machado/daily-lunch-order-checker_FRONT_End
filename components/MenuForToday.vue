666148724cd40fc7f865dc1e31afdc2729a45350
Cleidson <cleidson.mac@outlook.com>
2023-03-12 10:23:46 -0400

    Here I recreated the basic web design using DIV Attributes

--- components/MenuForToday.vue
+++ components/MenuForToday.vue
@@ -1,105 +1,60 @@
 <template>
-    <div>
-        <div>
-        <!-- START AVISO_OFF -->
-        <div class="grid grid-cols-3 gap-0" v-if="!menuList || !menuList.length">
-            <div>
-                <img class="object-scale-down h-40 w-96" src="alertWarningBlack.png"/>
-            </div>
-            <div class="col-span-2 p-0">
-                <br>
-                <br>
-                <span class="txt-warning">AVISO: Não há serviço de Marmitas aos Sábados e Domingos...</span>
-                <br>
-                <span class="txt-warning">Ou o Serviço Pode ter sido Cancelado Por Motivos afins da Gerência</span>
-                <br>
-                <span class="txt-warning">Ou, ainda, o MENU deste dia da Semana pode não ter sido Criado!</span>
-                <br>
-                <span class="txt-warning">Ou, API PODE ESTAR OFF LINE!!!</span>
-            </div>
-        </div>
-        <!-- END AVISO_OFF -->
+    <div class="pt-5 space-y-2 ...">
+        <div class="container max-w-screen-lg mx-auto px-1">
 
+            <div class="flex flex-row auto-cols-max auto-rows-auto gap-2 bg-gray-300 border-solid border-4 border-gray-300 rounded-md ...">
 
-        <!-- ########################################################################################################### -->
-
-
-        <!-- START DIV PRINCIPAL DOS ITENS DO DIA CORRENTE -->
-            <div class="flex flex-row">
-            <!-- START DESCRIÇÃO COMPLETA MARMITA E DO ESPAÇO DA FOTO 1 -->
-                <div class="basis-1/4" v-for="menu in menuList" v-bind:key="menu.id">
-                    <span>Imagem Ilustrativa!</span>
-                    <img class="img-food-item" v-bind:src="menu.lunchBox.imageLinkPath" v-if="menuList.length"/>
+                <div class="basis-96" v-for="menu in menuList" v-bind:key="menu.id">
+                    <img class="mt-1 mb-1 rounded-lg shadow-lg object-fill h-48 w-96 ..." v-bind:src="menu.lunchBox.imageLinkPath" v-if="menuList.length"/>
                 </div>
-            <!-- END DESCRIÇÃO COMPLETA MARMITA E DO ESPAÇO DA FOTO 1 -->
-                    <div class="basis-1/3 p-2">
-                        <!-- START DESCRIÇÃO COMPLETA MARMITA -->
-                            <div class="grid grid-cols-1 gap-0 p-1" v-for="menu in menuList" v-bind:key="menu.id">
-                                <div class="txt-label-item">Embalagem:</div>
-                                <div class="txt-title-food ">{{ menu.lunchBox.name }}</div>
-                                <p class="txt-content">{{ menu.lunchBox.description }}</p>
-                            </div>
-                        <!-- END DESCRIÇÃO COMPLETA MARMITA -->
-                    </div>
-            </div>
-        <!-- END DIV PRINCIPAL DOS ITENS DO DIA CORRENTE -->
 
-                <!-- START SPACER -->   
-                    <div class="col-span-3 text-center" >
-                        <hr class="w-auto h-1 mx-auto my-4 bg-gray-200 border-0 rounded md:my-1" v-if="menuList.length">
-                    </div>
-                <!-- END SPACER -->
-
-
-
-<!-- START DIV PRINCIPAL DOS ITENS DO DIA CORRENTE -->
-            <div class="flex flex-row">
-            <!-- START DESCRIÇÃO COMPLETA MARMITA E DO ESPAÇO DA FOTO 2 -->
-                <div class="basis-1/4" v-for="menu in menuList" v-bind:key="menu.id">
-                    <span>Imagem Ilustrativa!</span>
-                    <img class="img-food-item" v-bind:src="menu.imageLinkPath" v-if="menuList.length"/>
+                <div class="basis-full"  v-for="menu in menuList" v-bind:key="menu.id">
+                    <div class="txt-label-item">Embalagem:</div>
+                    <div class="txt-title-food ">{{ menu.lunchBox.name }}</div>
+                    <p class="txt-content">{{ menu.lunchBox.description }}</p>
                 </div>
-            <!-- END DESCRIÇÃO COMPLETA MARMITA E DO ESPAÇO DA FOTO 2 -->
-                    <div class="basis-1/3 p-2">
-                        <!-- START DESCRIÇÃO COMPLETA DO MENU -->
-                            <div class="grid grid-cols-1 gap-0 p-1" v-for="menu in menuList" v-bind:key="menu.id">
-                                <div class="txt-label-item">Cardápio:</div>
-                            <div class="txt-title-food">{{ menu.name }}</div>
-                            <div class="txt-content">{{ menu.description }}</div>
-                            </div>
-                        <!-- END DESCRIÇÃO COMPLETA DO MENU -->
-                    </div>
-            </div>
-<!-- END DIV PRINCIPAL DOS ITENS DO DIA CORRENTE -->
 
-
-            <!-- START SPACER -->
-            <div class="col-span-3   text-center">
-                <hr class="w-auto h-1 mx-auto my-4 bg-gray-200 border-0 rounded md:my-1" v-if="menuList.length">
-            </div>
-            <!-- END SPACER -->
-
-
-            <!-- START DADOS NUMÉRICOS -->
-        <div class="grid grid-cols-3 gap-0">
-            <div class="grid grid-cols-1 gap-0 mt-4 mb-4" v-for="menu in menuList" v-bind:key="menu.id">
-                <div class="txt-title-food text-center">CALORIAS MÉDIAS <span class="txt-label-item"> ( Cal )</span></div>
-                <div class="txt-content text-center">{{ menu.averageCalories }}</div>
-            </div>
-            
-            <div class="grid grid-cols-1 gap-0 mt-4 mb-4" v-for="menu in menuList" v-bind:key="menu.id">
-                <div class="txt-title-food text-center">PESO MÉDIO<span class="txt-label-item"> ( g )</span></div>
-                <div class="txt-content text-center">{{ menu.averageWeight }}</div>
             </div>
 
-            <div class="grid grid-cols-1 gap-0 mt-4 mb-4" v-for="menu in menuList" v-bind:key="menu.id">
-                <div class="txt-title-food text-center">PREÇO MÉDIO<span class="txt-label-item"> ( R$ )</span></div>
-                <div class="txt-content text-center">R$ {{ menu.averagePrice }}</div>
-            </div>
         </div>
-            <!-- END DADOS NUMÉRICOS -->
+
+        <div class="container max-w-screen-lg mx-auto px-1">
+
+            <div class="flex flex-row auto-cols-max auto-rows-auto gap-2 bg-gray-300 border-solid border-4 border-gray-300 rounded-md ...">
+
+                <div class="basis-96" v-for="menu in menuList" v-bind:key="menu.id">
+                    <img class="mt-1 mb-1 rounded-lg shadow-lg object-fill h-48 w-96 ..." v-bind:src="menu.imageLinkPath" v-if="menuList.length"/>
+                </div>
+
+                <div class="basis-full"  v-for="menu in menuList" v-bind:key="menu.id">
+                    <div class="txt-label-item">Cardápio:</div>
+                    <div class="txt-title-food">{{ menu.name }}</div>
+                    <div class="txt-content">{{ menu.description }}</div>
+                </div>
+
+            </div>
+
         </div>
-        
+
+        <div class="container max-w-screen-lg mx-auto px-1">
+            <div class="grid grid-cols-3 gap-0 bg-gray-300 border-solid border-4 border-gray-300 rounded-md ...">
+                <div class="grid grid-cols-1 gap-0 mt-4 mb-4" v-for="menu in menuList" v-bind:key="menu.id">
+                    <div class="txt-title-food text-center">CALORIAS MÉDIAS <span class="txt-label-item"> ( Cal )</span></div>
+                    <div class="txt-content-menu-numbers">{{ menu.averageCalories }}</div>
+                </div>
+                
+                <div class="grid grid-cols-1 gap-0 mt-4 mb-4" v-for="menu in menuList" v-bind:key="menu.id">
+                    <div class="txt-title-food text-center">PESO MÉDIO<span class="txt-label-item"> ( g )</span></div>
+                    <div class="txt-content-menu-numbers">{{ menu.averageWeight }}</div>
+                </div>
+
+                <div class="grid grid-cols-1 gap-0 mt-4 mb-4" v-for="menu in menuList" v-bind:key="menu.id">
+                    <div class="txt-title-food text-center">PREÇO MÉDIO<span class="txt-label-item"> ( R$ )</span></div>
+                    <div class="txt-content-menu-numbers">R$ {{ menu.averagePrice }}</div>
+                </div>
+            </div>
+                <!-- END DADOS NUMÉRICOS -->
+        </div>
     </div>
 </template>
 
@@ -112,7 +67,6 @@ export default Vue.extend({
 
     data: () => ({
         menuList: [],
-        localImg: 'ImageTestHoriz.png', //PENSAR MELHOR NISSO!!.. COMO TRABALHAR A EXIBIÇÃO DE IMG RESPECTIVA A CADA MENU E AFINS...
     }),
 
     created() {
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
     <div>
       <AppHeader />
           <Nuxt />  <!-- O CONTEÚDO VEM DESSE CARA (pages\index.vue) ELE TB CONTÉM A TABELA E O MENU -->
-      <AppFooter />
+      <!-- <AppFooter />  HERE-COMPONENT -->
     </div>
   </template>
   
