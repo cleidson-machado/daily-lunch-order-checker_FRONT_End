f53304f85145d35d9ad03178a58c5d13aafb56da
Cleidson <cleidson.mac@outlook.com>
2023-03-27 19:18:03 -0400

    Just Add some improvements on the input field

--- components/OrderModal1.vue
+++ components/OrderModal1.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="wrapper" class="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex" v-on:click="handleClose">
-    <div id="wrapperIn" class="w-[500px] mt-16 mx-auto">
-      <form class="bg-white rounded px-8 pt-6 pb-8 mb-4">
+    <div id="wrapperIn" class="w-[600px] mt-16 mx-auto">
+      <form id="form" class="bg-white rounded px-8 pt-6 pb-8 mb-4">
         <div class="mb-4">
           <h5 class="mb-0 text-base text-neutral-600 dark:text-neutral-200">
             <strong>Embalagem:</strong>
@@ -30,24 +30,26 @@
                 {{ user.firstName }} {{ user.lastName }}
             </strong>
           </p>
-          <div class="w-10 relative mb-4 flex flex-wrap items-stretch">
+          <div class="w-20 relative mb-4 flex flex-wrap items-stretch">
             <div class="relative inline-flex self-center">
-              <input type="text" 
-                class="text-sm 
+              <input id="searchField"
+                class="text-sm
                 font-semibold 
                 rounded border-2 
                 border-blue-400 
                 text-gray-600 
-                h-10 w-100 pl-5 pr-10 
+                flex-none w-96 h-auto pl-5 pr-10 
                 bg-white 
                 hover:border-gray-400 
                 focus:outline-none appearance-none"
-                placeholder="Dig. Sobre Nome ou Email..."
-                v-model="txtDataFindUser" v-if="userList.length === 0">
+                placeholder="Digite o Sobrenome ou Email na Pesquisa..."
+                v-model="txtDataToFindUser"
+                type="text"
+                v-if="userList.length === 0">
             </div>
           </div>
         </div>
-        <div class="flex items-center justify-between">
+        <div class="flex items-center justify-self-start">
           <button 
             v-on:click="fechUserDataByLastNameOrEmail"
             type="button"
@@ -82,6 +84,23 @@
             v-if="userList.length != 0">
             <span>FINALIZAR o PEDIDO!</span>
           </button>
+          <button 
+            v-on:click="clearForm"
+            type="button"
+            class="text-white 
+            bg-blue-700 
+            hover:bg-blue-80
+            0 focus:ring-4 
+            focus:ring-blue-300 
+            font-medium rounded-lg 
+            text-sm px-5 py-2.5 mr-2 mb-2 
+            dark:bg-blue-600 
+            dark:hover:bg-blue-700 
+            focus:outline-none 
+            dark:focus:ring-blue-800"
+            v-if="userList.length != 0">
+            <span>RETORNAR A BUSCA!</span>
+          </button>
         </div>
       </form>
     </div>
@@ -93,6 +112,8 @@ import Vue from 'vue'
 
   export default Vue.extend({
     name: 'OrderLunchModal',
+
+    el: '#form',
 
     data: () => {
         return {
@@ -114,27 +135,43 @@ import Vue from 'vue'
             selectedUserId: '',
 
             //LAST NAME OR EMAIL FOR SEARCH
-            txtDataFindUser: ''
+            txtDataToFindUser: '',
         }
     },
 
     mounted() {
+        //CREATE HERE TO POPULATE THE COMBO SELECT ON LOADING PAGE..
         //this.selectAllUserToOrder()
     },
 
     methods: {
 
+      //OK.. WORKING GOOD!!
+      clearForm(){
+        this.userList = [] //CLEAR THE DATA LIST OF USER FIND BY SEARCH
+        this.txtDataToFindUser = '' //CLEAR THE FIELD USED ON A SEARCH ACTION
+      },
+
       handleClose(e){
         if(e.target.id === 'wrapper' || e.target.id === 'wrapperIn') 
         {
           this.$emit('close-modal')
+          this.clearForm()
         }
       },
 
-      async saveTheNewOrderTEST(){
-        console.log('User_ID_Selected:' + this.userList[0].id)
+      //OK.. WORKING GOOD!!
+      async fechUserDataByLastNameOrEmail(){
+      await this.$axios.$get('/foodapi/user/listBy/lastNameOrEmail/'+ this.txtDataToFindUser)
+        .then( response => {
+          this.userList = response
+        })
+        .catch(err => {
+        console.log(err)
+        })
       },
 
+      //SAVE THE ORDER A LUNCH... MELHORAR!!???
       async saveTheNewOrder(){
         await this.$axios.$post('/foodapi/order-for-lunch/add', {
             orderValue: this.itemPrice,
@@ -155,17 +192,6 @@ import Vue from 'vue'
       //GET USER LIST FOR ORDER A LUNCH... TEST TO USE ON THE SELECT FIELD
       async selectAllUserToOrder() {
         await this.$axios.$get('/foodapi/user/listAll')
-        .then( response => {
-          this.userList = response
-        })
-        .catch(err => {
-        console.log(err)
-        })
-      },
-
-      //TEST...
-      async fechUserDataByLastNameOrEmail(){
-        await this.$axios.$get('/foodapi/user/listBy/lastNameOrEmail/'+ this.txtDataFindUser)
         .then( response => {
           this.userList = response
         })
