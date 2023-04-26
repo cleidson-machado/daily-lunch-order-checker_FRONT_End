a62f62e967d819baf977bd40aaa6080280e9baee
Cleidson <cleidson.mac@outlook.com>
2023-04-26 19:41:09 -0400

    OK.. REFACTORING THE CODE TO FIX THE FREEZE PROBLEM WHEN NAVIGATE PART 08 NUXT LINK THE CORRECT WAY

--- components/pageCodeGroups/forAdminPage/adminModal1.vue
+++ components/pageCodeGroups/forAdminPage/adminModal1.vue
@@ -58,21 +58,22 @@
                             </button>
                         </router-link>
 
-                        <router-link to="/menus" custom v-slot="{ navigate }">
-                            <button v-on:click="navigate" role="link" data-modal-hide="admin-modal"
-                                class="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-300 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                    class="bi bi-book-half" viewBox="0 0 16 16">
-                                    <path
-                                        d="M8.5 2.687c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492V2.687zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z" />
-                                </svg>
-                                <span class="flex-1 ml-3 whitespace-nowrap text-left ...">CARDÁPIOS</span>
-                                <span
-                                    class="inline-flex items-center justify-center px-2 py-0.5 ml-3 text-xs font-medium text-gray-500 bg-gray-200 rounded dark:bg-gray-700 dark:text-gray-400">
-                                    Adm Only!
-                                </span>
-                            </button>
-                        </router-link>
+
+                        <nuxt-link to="/menus"
+                            class="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-300 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white"
+                            data-modal-hide="admin-modal">
+                            <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                fill="currentColor" class="bi bi-book-half" viewBox="0 0 16 16">
+                                <path
+                                    d="M8.5 2.687c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492V2.687zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z" />
+                            </svg>
+                            <span class="flex-1 ml-3 whitespace-nowrap text-left ...">CARDÁPIOS</span>
+                            <span
+                                class="inline-flex items-center justify-center px-2 py-0.5 ml-3 text-xs font-medium text-gray-500 bg-gray-200 rounded dark:bg-gray-700 dark:text-gray-400">
+                                Adm Only!
+                            </span>
+                        </nuxt-link>
+
 
                         <router-link to="/orders" custom v-slot="{ navigate }">
                             <button v-on:click="navigate" role="link" data-modal-hide="admin-modal"
