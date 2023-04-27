4a99737cb581ee4256f3e731a1d27305f0ef9353
Cleidson <cleidson.mac@outlook.com>
2023-04-27 09:18:00 -0400

    TRY SECCOND DEPLOY AT NETFLY.. GIT INTEGRATIONS

--- components/inc/AppHeader.vue
+++ components/inc/AppHeader.vue
@@ -44,7 +44,7 @@
                 d="M3 6a1 1 0 00-1 1v10a1 1 0 001 1h8a1 1 0 001-1V7a1 1 0 00-1-1H3zm6.874 4.166a.75.75 0 10-1.248-.832l-2.493 3.739-.853-.853a.75.75 0 00-1.06 1.06l1.5 1.5a.75.75 0 001.154-.114l3-4.5z">
               </path>
             </svg>
-            <span>Meus Pedidos</span>
+            <span>Meus NOVOS Pedidos</span>
           </button>
 
           <button class="btn-disable mx-4">
